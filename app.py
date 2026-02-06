c007d571840e4876f6298017a5f5f6bb80d84c7d
Vinay Kumar Yadav <107872103+vinaykryadav1@users.noreply.github.com>
2026-02-07 00:31:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -158,4 +158,6 @@ def logout():
 
 # ================= START =================
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
+
